e1d0b85bf9ce2e80096a556395ab1647712b18f4
gabona07 <bonagabriella1995@gmail.com>
2021-07-28 10:00:40 +0200

    Connect view with the view model

--- app/src/main/java/com/smartlynx/wupassignment/MainActivity.kt
+++ app/src/main/java/com/smartlynx/wupassignment/MainActivity.kt
@@ -2,10 +2,30 @@ package com.smartlynx.wupassignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.smartlynx.wupassignment.model.CardInfo
+import com.smartlynx.wupassignment.viewmodel.MainActivityViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initialiseViewModel()
+    }
+
+    private fun initialiseViewModel() {
+        val viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        viewModel.getCardsObserver().observe(this, Observer<ArrayList<CardInfo>> {
+            if (it != null) {
+                // recyclerViewAdapter.setItemsData(it.items)
+                Log.d("hello", "initialiseViewModel: $it")
+            } else {
+                Toast.makeText(this, "Could not load card data.", Toast.LENGTH_SHORT).show()
+            }
+        })
+        viewModel.makeApiCall()
     }
 }
